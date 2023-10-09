4425a0735ee81ace372cf18e454b2af0b75ae8ff
Dennis Kigen <kigen.work@gmail.com>
2023-10-09 23:01:16 +0300

    Fix test script

--- projects/ngx-file-uploader/src/lib/ngx-file-uploader.module.ts
+++ projects/ngx-file-uploader/src/lib/ngx-file-uploader.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WebcamModule } from 'ngx-webcam';
@@ -9,5 +9,6 @@ import { NgxFileUploaderComponent } from './ngx-file-uploader.component';
   imports: [CommonModule, FormsModule, WebcamModule],
   declarations: [NgxFileUploaderComponent],
   exports: [NgxFileUploaderComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class NgxFileUploaderModule {}
